80b928a73cca4a052ff7dca3677741bcf7568531
Gino Zaccardelli <ginozacc@gmail.com>
2020-09-07 16:09:56 -0400

    View functionality added, condensed project to single file (tracker.js deprecated)

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
+const cTable = require("console.table");
+const inq = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -16,14 +18,68 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    afterConnection();
 });
 
-function afterConnection() {
-    connection.query("SELECT * FROM employee", function (err, res) {
-        if (err) throw err;
-        console.log(res);
-        connection.end();
+function view() {
+    inq.prompt([
+        {
+            type: "list",
+            message: "Select which table to view:",
+            name: "view",
+            choices: ["Employees", "Roles", "Departments"]
+        }
+    ]).then(response => {
+        switch (response.view) {
+            case "Employees":
+                connection.query("SELECT * FROM employee", function (err, res) {
+                    if (err) throw err;
+                    console.table(res);
+                    prompt();
+                });
+                break;
+            case "Roles":
+                connection.query("SELECT * FROM role", function (err, res) {
+                    if (err) throw err;
+                    console.table(res);
+                    prompt();
+                });
+                break;
+            case "Departments":
+                connection.query("SELECT * FROM department", function (err, res) {
+                    if (err) throw err;
+                    console.table(res);
+                    prompt();
+                });
+                break;
+        };
     });
 };
+
+
+function prompt() {
+    inq.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "toDoAction",
+            choices: ["View...", "Add...", "Update..."]
+        }
+    ]).then(response => {
+        switch (response.toDoAction) {
+            case "View...":
+                view();
+                break;
+            case "Add...":
+                //add function
+                break;
+            case "Update...":
+                //update function
+                break;
+            default:
+                //view function
+                break;
+        };
+    });
+};
+
+prompt();
